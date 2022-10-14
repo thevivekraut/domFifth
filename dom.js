79e1628c876e575456ad36f41ce1c4e08f6394ce
thevivekraut <vraut2306@gmail.com>
2022-10-14 11:44:50 +0530

    DOMfive

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+var li  = document.getElementsByClassName('new-list-item');
+li[0].style.backgroundColor = 'green';
+li[0].style.fontWeight = 'bold';
+
+
+var tag = document.getElementsByTagName('li');
+tag[5].style.backgroundColor = 'orange';
+tag[5].style.fontWeight = 'bold';
+
+console.log('Added New Elements With Different Class name And Edited ByClassName ANd ById')
